71c38b3ef5c6447f4d10fe284a5d722147e14f99
Christopher Vestman <christophervestman@gmail.com>
2022-04-25 23:43:36 +0200

    add: social media links

--- src/Components/Employees/Employees.tsx
+++ src/Components/Employees/Employees.tsx
@@ -1,14 +1,11 @@
 import axios from "axios"
 import React, { useEffect, useState } from "react"
 import { Employee } from "../../types"
-
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faTwitter, faLinkedin, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
 
 const url = process.env.REACT_APP_SECRET_URL as string;
 const header = process.env.REACT_APP_SECRET_HEADER as string
-
-
 
 export const EmployeeList = () => {
     const [employees, setEmployees] = useState<Employee[]>([])
@@ -30,18 +27,28 @@ export const EmployeeList = () => {
     return (
         <div>
             <h1 className="font-bold text-4xl">Wall of Ninjas</h1>
-            <div className="grid gap-4 grid-cols-4">
+            <div className="grid gap-4 grid-cols-4 p-10">
             {
                 employees.map((employee, index) => (
                     <div key={employee.name} className="max-w-xs rounded-lg shadow-lg">
-                        <img key={employee.imagePortraitUrl} className="w-auto h-fit pt-4" src={`${employee.imagePortraitUrl}`} alt={`img of ${employee.name}`} />
+                        { 
+                            (employee.imagePortraitUrl === null)
+                                ?   <img key={employee.imagePortraitUrl} className="w-fit h-fit pt-4" src="https://via.placeholder.com/320x440" alt={`placeholder for ${employee.name}`} />
+                                :   <img key={employee.imagePortraitUrl} className="w-fit h-fit pt-4" src={`${employee.imagePortraitUrl}`} alt={`img of ${employee.name}`} /> 
+                        }
                         <div className="grid grid-cols-2 p-4" >
                             <div key={employee.name} className="grid grid-rows-2 grid-flow-col text-left">
                                 <div>{employee.name} </div>
                                 <div>Office: {employee.office}</div>
                             </div>
-                            <div key={index} className="text-right">
-                                {employee.gitHub} {employee.linkedIn} {employee.twitter}
+                            <div key={index} className="text-right mx-1">
+                                
+                                <span>
+                                    {employee.linkedIn && <a href={`https://www.linkedin.com${employee.linkedIn}`}><FontAwesomeIcon icon={faLinkedin} size="lg"/></a>}&nbsp;
+                                    {employee.gitHub && <a href={`https://www.github.com/${employee.gitHub}`}><FontAwesomeIcon icon={faGithub} size="lg"/></a>}&nbsp;
+                                    {employee.twitter && <a href={`https://www.twitter.com/${employee.twitter}`}><FontAwesomeIcon icon={faTwitter} size="lg"/></a>}&nbsp;
+                                    {employee.stackOverflow && <a href={`https://www.stackoverflow.com/users/${employee.stackOverflow}`}><FontAwesomeIcon icon={faStackOverflow} size="lg"/></a>}
+                                </span>
                             </div>
                         </div>
                     </div>
